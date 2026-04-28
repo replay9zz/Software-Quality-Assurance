731ccea7dbce75477d3ad773063b9044ad98be03
replay9zz <xiexiecuisite@gmail.com>
2026-04-28 17:15:54 +0900

    Update

--- test2.cpp
+++ test2.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
     int tt;
     cin >> tt;
+    Quiz2 q2;
     while (tt--) {
         int X, Y;
         cin >> X >> Y;
-        Quiz2 q2;
         q2.program(X, Y);
     }
 
